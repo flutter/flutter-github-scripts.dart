ed9cbb2f79193b0dace29102791ef9ec63a366ff
Ray Rischpater, KF6GPE <rischpater@google.com>
2020-04-23 15:16:02 -0700

    Correctly use to date stamps when computing results.

--- bin/today_report.dart
+++ bin/today_report.dart
@@ -37,7 +37,7 @@ void printHeader(Options opts) {
   var fromStamp = opts.from.toIso8601String().substring(0,10);
   var toStamp = opts.to.toIso8601String().substring(0,10);
 
-  print('\n\nTo: flutter-team@google.com, flutter-dart-tpm@gooogle.com\n');
+  print('\n\nTo: flutter-team@google.com, flutter-dart-tpm@gooogle.com\\n');
   if (DateTime.now().weekday == DateTime.tuesday) print('Subject: Flutter TODAY Tuesday report!\n');
   if (DateTime.now().weekday == DateTime.thursday) print('Subject: Flutter TODAY Thursday report!\n');
   if (DateTime.now().weekday != DateTime.tuesday &&
@@ -63,8 +63,8 @@ void main(List<String> args) async {
 
   issues.forEach((issue) {
     if (issue.state == "OPEN") open.add(issue);
-    if (issue.createdAt.compareTo(opts.from) > 0) openedThisPeriod.add(issue);
-    if (issue.state == "CLOSED" && issue.closedAt.compareTo(opts.from) > 0 ) closedThisPeriod.add(issue);
+    if (issue.createdAt.compareTo(opts.from) >= 0 && issue.createdAt.compareTo(opts.to) <= 0) openedThisPeriod.add(issue);
+    if (issue.state == "CLOSED" && issue.closedAt.compareTo(opts.from) >= 0 && issue.closedAt.compareTo(opts.to) <= 0) closedThisPeriod.add(issue);
   });
   
   var fromStamp = opts.from.toIso8601String().substring(0,10);
