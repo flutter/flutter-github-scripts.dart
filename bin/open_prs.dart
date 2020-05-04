912f78d0b8cb9932dc9d331d2c53c0f7f5a496a9
Ray Rischpater, KF6GPE <rischpater@google.com>
2020-05-04 15:58:53 -0700

    First cut at pulling PRs

---
+++ bin/open_prs.dart
@@ -0,0 +1,70 @@
+import 'package:graphql/client.dart';
+import 'package:flutter_github_scripts/github_datatypes.dart';
+import 'package:flutter_github_scripts/github_queries.dart';
+import 'package:args/args.dart';
+import 'dart:io';
+
+
+
+class Options  {
+  final _parser = ArgParser(allowTrailingOptions: false);
+  ArgResults _results;
+  int get exitCode => _results == null ? -1 : _results['help'] ? 0 : null;
+
+  Options(List<String> args) {
+    _parser
+      ..addFlag('help', defaultsTo: false, abbr: 'h', negatable: false, help: 'get usage')
+    try {
+      _results = _parser.parse(args);
+      if (_results['help'])  _printUsage();
+    } on ArgParserException catch (e) {
+      print(e.message);
+      _printUsage();
+    }
+  }
+
+  void _printUsage() {
+    print('Usage: pub run open_prs.dart');
+    print(_parser.usage);
+  }
+}
+
+
+void main(List<String> args) async {
+  final opts = Options(args);
+  if (opts.exitCode != null) exit(opts.exitCode);
+  final token = Platform.environment['GITHUB_TOKEN'];
+  final github = Github(token);
+  
+  var issues = await github.issues(owner: 'flutter', 
+    name: 'flutter', 
+    filterSpec: 'labels: ["⚠ TODAY"]');
+  
+  var open = List<Issue>();
+  var openedThisPeriod = List<Issue>();
+  var closedThisPeriod = List<Issue>();
+
+  issues.forEach((issue) {
+    if (issue.state == "OPEN") open.add(issue);
+    if (issue.createdAt.compareTo(opts.from) >= 0 && issue.createdAt.compareTo(opts.to) <= 0) openedThisPeriod.add(issue);
+    if (issue.state == "CLOSED" && issue.closedAt.compareTo(opts.from) >= 0 && issue.closedAt.compareTo(opts.to) <= 0) closedThisPeriod.add(issue);
+  });
+  
+  var fromStamp = opts.from.toIso8601String().substring(0,10);
+  var toStamp = opts.to.toIso8601String().substring(0,10);
+
+  printHeader(opts);
+
+  print('This shows the number of new, open, and closed `TODAY` issues over the period from');
+  print('${fromStamp} to ${toStamp}.\n\n');
+
+  print('### ${open.length} open `TODAY` issue(s)');
+  open.forEach((issue) => print(issue.summary(boldInteresting: false, linebreakAfter: true)));
+
+  print('### ${openedThisPeriod.length} `TODAY` issues opened between ${fromStamp} and ${toStamp}');
+  openedThisPeriod.forEach((issue) => print(issue.summary(boldInteresting: false, linebreakAfter: true)));
+
+  print('### ${closedThisPeriod.length} `TODAY` issues closed between ${fromStamp} and ${toStamp}');
+  closedThisPeriod.forEach((issue) => print(issue.summary(boldInteresting: false, linebreakAfter: true)));
+
+}
--- lib/github_queries.dart
+++ lib/github_queries.dart
@@ -2,6 +2,7 @@ import 'dart:collection';
 import 'dart:io';
 import 'package:graphql/client.dart';
 import 'package:flutter_github_scripts/github_datatypes.dart';
+import 'package:quiver/core.dart' show hash2;
 // Filter by label: 'labels: ["⚠ TODAY"]'
 
 
@@ -45,18 +46,20 @@ class Github {
             }
     ''';
    
-    List<Issue> result = List<Issue>();
-    bool done = false;
-    String after = 'null';
+    var result = List<Issue>();
+    var done = false;
+    var after = 'null';
     do {
       var query = _query_issues
         .replaceAll(r'${repositoryOwner}', owner)
         .replaceAll(r'${repositoryName}', name)
         .replaceAll(r'${after}', after)
         .replaceAll(r'${filter}', filter)
-        .replaceAll(r'${issueResponse}', Issue.jqueryResponse);
+        .replaceAll(r'${issueResponse}', Issue.jqueryResponse)
+        .replaceAll(r'${pageInfoResponse}', _PageInfo.jqueryResponse);
 
       final options = QueryOptions(document: query);
+
       final page = await _client.query(options);
       if (page.hasErrors) {
         throw(page.errors.toString());
@@ -68,8 +71,10 @@ class Github {
         result.add(issue);
       });
  
-      done = !page.data['repository']['issues']['pageInfo']['hasNextPage'];
-      if (!done) after = '"${page.data['repository']['issues']['pageInfo']['endCursor']}"';
+      _PageInfo pageInfo = _PageInfo.fromGraphQL(page.data['repository']['issues']['pageInfo']);
+
+      done = !pageInfo.hasNextPage;
+      if (!done) after = '"${pageInfo.endCursor}"';
 
     } while( !done );
 
@@ -93,16 +98,50 @@ class Github {
       return PullRequest.fromGraphQL(page.data['repository']['pullRequest']);
   }
 
-  
-  final _query_pullRequest = 
-  r'''
-  query { 
-    repository(owner:"${repositoryOwner}", name:"${repositoryName}") {
-      pullRequest(number:${number}) 
-      ${pullRequestResponse}
-    }
+Future<List<PullRequest>> pullRequests({String owner, String name, List<String> states = null}) async {
+    var stateList = states == null ? '' : 
+    '''
+    states: ${states}
+    ''';
+   
+    var result = List<PullRequest>();
+    var done = false;
+    var after = 'null';
+    do {
+      var query = _query_pullRequests
+        .replaceAll(r'${repositoryOwner}', owner)
+        .replaceAll(r'${repositoryName}', name)
+        .replaceAll(r'${after}', after)
+        .replaceAll(r'${states}', stateList)
+        .replaceAll(r'${issueResponse}', Issue.jqueryResponse)
+        .replaceAll(r'${pageInfoResponse}', _PageInfo.jqueryResponse);
+
+      final options = QueryOptions(document: query);
+
+      final page = await _client.query(options);
+      if (page.hasErrors) {
+        throw(page.errors.toString());
+      }
+
+      var edges = page.data['repository']['issues']['edges'];
+      edges.forEach((edge) {
+        var pullRequest = PullRequest.fromGraphQL(edge['node']);
+  print(pullRequest);
+        result.add(pullRequest);
+      });
+ 
+      _PageInfo pageInfo = _PageInfo.fromGraphQL(page.data['repository']['issues']['pageInfo']);
+
+      done = !pageInfo.hasNextPage;
+      if (!done) after = '"${pageInfo.endCursor}"';
+
+
+
+    } while( !done );
+
+    return result;
   }
-  ''';
+
 
   final _query_issue = 
   r'''
@@ -121,12 +160,10 @@ class Github {
     repository(owner:"${repositoryOwner}", name:"${repositoryName}") {
       issues(first: 25, 
         after: ${after}, 
-        ${filter})
+        ${stateList})
       {
         totalCount,
-        pageInfo {
-          startCursor, hasNextPage, endCursor
-        },
+        pageInfo ${pageInfoResponse}
         edges {
           node ${issueResponse}
         }
@@ -135,6 +172,71 @@ class Github {
   }
   ''';
 
+  final _query_pullRequest = 
+  r'''
+  query { 
+    repository(owner:"${repositoryOwner}", name:"${repositoryName}") {
+      pullRequest(number:${number}) 
+      ${pullRequestResponse}
+    }
+  }
+  ''';
+
+  final _query_pullRequests = 
+  r'''
+  query { 
+    repository(owner:"${repositoryOwner}", name:"${repositoryName}") {
+      issues(first: 25, 
+        after: ${after}, 
+        states: ${states})
+      {
+        totalCount,
+        pageInfo ${pageInfoResponse}
+        edges {
+          node ${pullRequestResponse}
+        }
+      }
+    }
+  }
+  ''';
+}
 
 
+class _PageInfo {
+  String _startCursor;
+  get startCursor => _startCursor;
+  bool _hasNextPage;
+  get hasNextPage => _hasNextPage;
+  String _endCursor;
+  get endCursor => _endCursor;
+  _PageInfo(this._startCursor, this._endCursor, this._hasNextPage);
+  static _PageInfo fromGraphQL(dynamic node) {
+    return _PageInfo(node['startCursor'], node['endCursor'], node['hasNextPage']);
+  }
+
+  String toString() {
+    return 'start: ${startCursor}, end: ${endCursor}, more? ${hasNextPage}';
+  }
+
+  @override
+  bool operator==(Object other) =>
+    identical(this, other) ||
+    other is _PageInfo &&
+    runtimeType == other.runtimeType &&
+    _startCursor == other._startCursor &&
+    _endCursor == other._endCursor &&
+    _hasNextPage == other._hasNextPage;
+
+  @override 
+  int get hashCode =>  hash2(
+      hash2(_startCursor.hashCode, _endCursor.hashCode),
+        _hasNextPage.hashCode);
+
+
+  static final jqueryResponse = 
+  r'''
+  {
+    startCursor, hasNextPage, endCursor
+  },
+  ''';
 }
