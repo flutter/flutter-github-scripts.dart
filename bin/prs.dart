1e22a1b24a8b92d93973c601fa6c74447bfed29f
Ray Rischpater, KF6GPE <rischpater@google.com>
2020-07-21 12:45:02 -0700

    Include count of PRs enumerated

--- bin/prs.dart
+++ bin/prs.dart
@@ -4,9 +4,7 @@ import 'package:flutter_github_scripts/github_queries.dart';
 import 'package:args/args.dart';
 import 'dart:io';
 
-
-
-class Options  {
+class Options {
   final _parser = ArgParser(allowTrailingOptions: false);
   ArgResults _results;
   bool get showClosed => _results['closed'];
@@ -19,16 +17,32 @@ class Options  {
 
   Options(List<String> args) {
     _parser
-      ..addFlag('help', defaultsTo: false, abbr: 'h', negatable: false, help: 'get usage')
-      ..addFlag('closed', defaultsTo: false, abbr: 'c', negatable: false, help: 'show closed PRs in date range')
-      ..addFlag('merged', defaultsTo: false, abbr: 'm', negatable: false, help: 'show merged PRs in date range')
-      ..addFlag('tsv', defaultsTo: false, abbr: 't', negatable: true, help: 'show results as TSV')
-      ..addOption('label', defaultsTo: null, abbr: 'l', help: 'only issues with this label');
+      ..addFlag('help',
+          defaultsTo: false, abbr: 'h', negatable: false, help: 'get usage')
+      ..addFlag('closed',
+          defaultsTo: false,
+          abbr: 'c',
+          negatable: false,
+          help: 'show closed PRs in date range')
+      ..addFlag('merged',
+          defaultsTo: false,
+          abbr: 'm',
+          negatable: false,
+          help: 'show merged PRs in date range')
+      ..addFlag('tsv',
+          defaultsTo: false,
+          abbr: 't',
+          negatable: true,
+          help: 'show results as TSV')
+      ..addOption('label',
+          defaultsTo: null, abbr: 'l', help: 'only issues with this label');
     try {
       _results = _parser.parse(args);
-      if (_results['help'])  _printUsage();
-      if ((_results['closed'] || _results['merged']) && _results.rest.length != 2 ) throw('need start and end dates!');
-      if (_results['merged'] && _results['closed']) throw('--merged and --closed are mutually exclusive!');
+      if (_results['help']) _printUsage();
+      if ((_results['closed'] || _results['merged']) &&
+          _results.rest.length != 2) throw ('need start and end dates!');
+      if (_results['merged'] && _results['closed'])
+        throw ('--merged and --closed are mutually exclusive!');
     } on ArgParserException catch (e) {
       print(e.message);
       _printUsage();
@@ -36,7 +50,8 @@ class Options  {
   }
 
   void _printUsage() {
-    print('Usage: pub run prs.dart [--tsv] [--label] [--closed fromDate toDate] [--merged fromDate toDate] [--closed fromDate toDate]');
+    print(
+        'Usage: pub run prs.dart [--tsv] [--label] [--closed fromDate toDate] [--merged fromDate toDate] [--closed fromDate toDate]');
     print('Prints PRs in flutter/flutter, flutter/engine repositories.');
     print('  --merged and --closed are mutually exclusive');
     print('  Dates are in ISO 8601 format');
@@ -57,31 +72,35 @@ void main(List<String> args) async {
   DateRange when = null;
   var rangeType = GitHubDateQueryType.none;
   if (opts.showClosed || opts.showMerged) {
-    state = opts.showClosed ?  GitHubIssueState.closed : GitHubIssueState.merged;
+    state = opts.showClosed ? GitHubIssueState.closed : GitHubIssueState.merged;
     when = DateRange(DateRangeType.range, start: opts.from, end: opts.to);
     rangeType = GitHubDateQueryType.merged;
   }
- 
-  for(var repo in repos) {
-    var prs = await github.fetch(owner: 'flutter', 
-      name: repo, 
-      type: GitHubIssueType.pullRequest,
-      state: state,
-      dateQuery: rangeType,
-      dateRange: when
-    );
-    
+
+  for (var repo in repos) {
+    var prs = await github.fetch(
+        owner: 'flutter',
+        name: repo,
+        type: GitHubIssueType.pullRequest,
+        state: state,
+        dateQuery: rangeType,
+        dateRange: when);
 
     var headerDelimiter = opts.tsv ? '' : '## ';
     var type = 'Open';
     if (opts.showMerged) type = 'Merged';
     if (opts.showClosed) type = 'Closed';
-    print("${headerDelimiter}${type} PRs in flutter/${repo} from " + opts.from.toIso8601String() + ' to ' + opts.to.toIso8601String());
+    print("${headerDelimiter}${type} PRs in flutter/${repo} from " +
+        opts.from.toIso8601String() +
+        ' to ' +
+        opts.to.toIso8601String());
+    print("There were ${prs.length} pull requests");
 
     if (opts.tsv) print(PullRequest.tsvHeader);
-    for(var pr in prs) {
+    for (var pr in prs) {
       if (opts.label != null && !pr.labels.containsString(opts.label)) continue;
-      var pullRequestString = opts.tsv ? pr.toTsv() : pr.summary(linebreakAfter: true);
+      var pullRequestString =
+          opts.tsv ? pr.toTsv() : pr.summary(linebreakAfter: true);
       print(pullRequestString);
     }
   }
