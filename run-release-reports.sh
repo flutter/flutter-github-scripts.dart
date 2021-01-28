de5e190bd19370e374ebed03b271053dce0586f5
Ray Rischpater, KF6GPE <kf6gpe@lothlorien.com>
2021-01-28 12:51:04 -0800

    remove extraneous space

--- run-release-reports.sh
+++ run-release-reports.sh
@@ -4,7 +4,7 @@
 FROM="2020-09-10T18:17:00";
 TO=$1 
 
-[ -z "$TO" ] && TO=` date --iso-8601=seconds`
+[ -z "$TO" ] && TO=`date --iso-8601=seconds`
 
 echo "Running release reports from ${FROM} to ${TO}"
 
