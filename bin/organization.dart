72c64d5561582a684adec2c6f4735f481dd5db68
Ray Rischpater, KF6GPE <rischpater@google.com>
2021-02-19 15:24:20 -0800

    Fix warning message.

--- bin/organization.dart
+++ bin/organization.dart
@@ -4,22 +4,25 @@ import 'package:flutter_github_scripts/github_queries.dart';
 import 'package:args/args.dart';
 import 'dart:io';
 
-
-
-class Options  {
+class Options {
   final _parser = ArgParser(allowTrailingOptions: false);
   ArgResults _results;
   String get login => _results.rest[0];
-  int get exitCode => _results == null ? -1 : _results['help'] ? 0 : null;
+  int get exitCode => _results == null
+      ? -1
+      : _results['help']
+          ? 0
+          : null;
   bool get tsv => _results['tsv'];
   Options(List<String> args) {
     _parser
-      ..addFlag('help', defaultsTo: false, abbr: 'h', negatable: false, help: 'get usage');
-      //..addFlag('tsv', defaultsTo: false, abbr: 't', negatable: true, help: 'show results as TSV');
+      ..addFlag('help',
+          defaultsTo: false, abbr: 'h', negatable: false, help: 'get usage');
+    //..addFlag('tsv', defaultsTo: false, abbr: 't', negatable: true, help: 'show results as TSV');
     try {
       _results = _parser.parse(args);
-      if (_results['help'])  _printUsage();
-      if (_results.rest.length != 1 ) throw('invalid id!');
+      if (_results['help']) _printUsage();
+      if (_results.rest.length != 1) throw ('invalid organization!');
     } on ArgParserException catch (e) {
       print(e.message);
       _printUsage();
@@ -38,7 +41,7 @@ void main(List<String> args) async {
   if (opts.exitCode != null) exit(opts.exitCode);
   final token = Platform.environment['GITHUB_TOKEN'];
   final github = GitHub(token);
-  
+
   var org = await github.organization(opts.login);
 
   print('id:\t${org.id}');
