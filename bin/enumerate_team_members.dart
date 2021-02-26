ef2324cafc86a4cd6e088315fb5d3d2d6f718e5b
Ray Rischpater, KF6GPE <rischpater@google.com>
2021-02-26 15:55:42 -0800

    Base implementation sans Googler tests; need to desk check data.

--- bin/enumerate_team_members.dart
+++ bin/enumerate_team_members.dart
@@ -55,6 +55,45 @@ class MemberInfo {
   MemberInfo(this._login);
 }
 
+enum When { first, last }
+
+findWhen(dynamic item, String login, When w) {
+  DateTime result;
+  switch (w) {
+    case When.first:
+      if (item.author?.login == login) {
+        result = item.createdAt;
+      } else {
+        // Maybe they're in the timeline?
+        if (item.timeline != null)
+          for (var timelineItem in item.timeline.timeline) {
+            if (timelineItem.actor != null &&
+                timelineItem.actor.login == login) {
+              result = timelineItem.createdAt;
+              break;
+            }
+          }
+        // The timeline can be incomplete; if so, just use the creation date.
+        if (result == null) result = item.createdAt;
+      }
+      break;
+
+    case When.last:
+      // Timelines happen after creation.
+      if (item.timeline != null)
+        for (var timelineItem in item.timeline.timeline) {
+          if (timelineItem.actor != null && timelineItem.actor.login == login) {
+            result = timelineItem.createdAt;
+            break;
+          }
+        }
+      // The timeline can be incomplete; if so, just use the creation date.
+      if (result == null) result = item.createdAt;
+      break;
+  }
+  return result;
+}
+
 void main(List<String> args) async {
   final opts = Options(args);
   if (opts.exitCode != null) exit(opts.exitCode);
@@ -77,27 +116,95 @@ void main(List<String> args) async {
 
   // Now go back and find out when they contributed
   for (var login in allMembers.keys) {
-    var earliestQuery = 'org:flutter involves:${login} sort:updated-asc';
-    var earlistItem = await github.searchIssuePRs(earliestQuery).isEmpty
-        ? null
-        : await github.searchIssuePRs(earliestQuery).first;
-    allMembers[login].firstContribution = earlistItem;
-    var latestQuery = 'org:flutter involves:${login} sort:updated-desc';
-    var latestItem = await github.searchIssuePRs(latestQuery).isEmpty
-        ? null
-        : await github.searchIssuePRs(latestQuery).first;
-    allMembers[login].lastContribution = latestItem;
+    var member = allMembers[login];
+    var earliestQueryAuthor = 'org:flutter author:${login} sort:updated-asc';
+    var earliestQueryCommenter =
+        'org:flutter commenter:${login} sort:updated-asc';
+
+    var earliestAuthored;
+    try {
+      earliestAuthored = await github.searchIssuePRs(earliestQueryAuthor).first;
+    } catch (e) {}
+
+    var earliestCommented;
+    try {
+      earliestCommented =
+          await github.searchIssuePRs(earliestQueryCommenter).first;
+    } catch (e) {}
+
+    if (earliestAuthored == null && earliestCommented == null) {
+      member.firstContributed = null;
+      member.firstContribution = null;
+    } else if (earliestAuthored == null && earliestCommented != null) {
+      member.firstContributed = findWhen(earliestCommented, login, When.first);
+      member.firstContribution = earliestCommented;
+    } else if (earliestAuthored != null && earliestCommented == null) {
+      member.firstContributed = findWhen(earliestAuthored, login, When.first);
+      member.firstContribution = earliestAuthored;
+    } else if (findWhen(earliestCommented, login, When.first)
+        .isBefore(findWhen(earliestAuthored, login, When.first))) {
+      member.firstContributed = findWhen(earliestCommented, login, When.first);
+      member.firstContribution = earliestCommented;
+    } else {
+      member.firstContributed = findWhen(earliestAuthored, login, When.first);
+      member.firstContribution = earliestAuthored;
+    }
+
+    var latestQueryAuthor = 'org:flutter author:${login} ';
+    var latestQueryCommenter =
+        'org:flutter commenter:${login} sort:updated-asc';
+
+    var latestAuthored;
+    try {
+      latestAuthored = await github.searchIssuePRs(latestQueryAuthor).first;
+    } catch (e) {}
+
+    var latestCommented;
+    try {
+      latestCommented = await github.searchIssuePRs(latestQueryCommenter).first;
+    } catch (e) {}
+
+    if (latestAuthored == null && latestCommented == null) {
+      member.firstContributed = null;
+      member.firstContribution = null;
+    } else if (latestAuthored == null && latestCommented != null) {
+      member.lastContributed = findWhen(latestCommented, login, When.last);
+      member.lastContribution = latestCommented;
+    } else if (latestAuthored != null && latestCommented == null) {
+      member.lastContributed = findWhen(latestAuthored, login, When.last);
+      member.lastContribution = latestAuthored;
+    } else if (findWhen(latestCommented, login, When.last)
+        .isAfter(findWhen(latestAuthored, login, When.last))) {
+      member.lastContributed = findWhen(latestCommented, login, When.last);
+      member.lastContribution = latestCommented;
+    } else {
+      member.lastContributed = findWhen(latestAuthored, login, When.last);
+      member.lastContribution = latestAuthored;
+    }
   }
 
   print('Teams and members in the ${opts.login} organization');
-  print('Team\tGithub login\tEarliest contribution\tLatest contribution');
+  print(
+      'Team\tGithub login\tFirst contributed\tEarliest contribution\tLast contributed\tLatest contribution');
   for (var team in membersByTeam.keys) {
     if (!opts.alwaysIncludeTeam) print('${team}');
     for (var member in membersByTeam[team]) {
       var row = opts.alwaysIncludeTeam ? '${team}\t${member}' : '\t${member}';
       var contributor = allMembers[member];
-      row +=
-          '\t${contributor.firstContribution?.url}\t${contributor.lastContribution?.url}';
+      if (contributor.firstContribution == null) {
+        row += '\t\t';
+      } else {
+        row += '\t${contributor.firstContributed?.toIso8601String()}';
+        row +=
+            '\t=HYPERLINK("${contributor.firstContribution?.url}","${contributor.firstContribution?.number}")';
+      }
+      if (contributor.lastContribution == null) {
+        row += '\t\t';
+      } else {
+        row += '\t${contributor.lastContributed?.toIso8601String()}';
+        row +=
+            '\t=HYPERLINK("${contributor.lastContribution?.url}","${contributor.lastContribution?.number}")';
+      }
       print(row);
     }
   }
