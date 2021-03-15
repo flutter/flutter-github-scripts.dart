c12bd59c669d03c9fc03b51bd7d2b99c49139cac
Ray Rischpater, KF6GPE <rischpater@google.com>
2021-03-15 16:12:58 -0700

    Add finding mean-to-close to table view.

--- README.md
+++ README.md
@@ -277,10 +277,12 @@ Usage: `dart bin/search [--tsv] github query`
 Returns the first 1,000 results of the given github query in either TSV or markdown format.
 
 ## priority_over_time.dart
-Usage `dart priority_over_time.dart [--queries] [--summarize] --from date --to date`
+Usage `dart priority_over_time.dart [--queries] [--customers] [--summarize] --from date --to date`
 
 Returns a summary of open and closed P0s, P1s, and P2s weekly over the given span.
-If --summaries is passed, reports in TSV; otherwise markdown.
-If --queries is past, shows each search term used for the open and closed queries.
+If `--summaries` is passed, reports in TSV; otherwise markdown.
+If `--queries` is passed, shows each search term used for the open and closed queries.
+if `--customers`is passed, shows only issues that also have at least one customer label.
+
 
 
--- bin/priority_over_time.dart
+++ bin/priority_over_time.dart
@@ -11,6 +11,7 @@ class Options {
   DateTime get to => DateTime.parse(_results['to']);
   bool get summarize => _results['summarize'];
   bool get showQueries => _results['queries'];
+  bool get onlyCustomers => _results['customers'];
   int get exitCode => _results == null
       ? -1
       : _results['help']
@@ -34,7 +35,11 @@ class Options {
           defaultsTo: false,
           help: 'Show only summary tables in markdown format.')
       ..addFlag('queries',
-          abbr: 'q', defaultsTo: false, help: 'Show queries used');
+          abbr: 'q', defaultsTo: false, help: 'Show queries used')
+      ..addFlag('customers',
+          abbr: 'c',
+          defaultsTo: false,
+          help: 'Only show issues with customer labels');
     try {
       _results = _parser.parse(args);
       if (_results['help']) _printUsage();
@@ -46,8 +51,57 @@ class Options {
 
   void _printUsage() {
     print(
-        'Usage: pub run priority_over_time.dart [--queries] [--summarize] --from date --to date');
+        'Usage: pub run priority_over_time.dart [--queries]  [--customers] [--summarize] --from date --to date');
     print(_parser.usage);
+  }
+}
+
+int countWithOrWithoutCustomers(List<dynamic> issues, {bool onlyCustomers}) {
+  var count = issues.length;
+  if (onlyCustomers) {
+    bool hasCustomer = false;
+    for (var item in issues) {
+      var issue = item as Issue;
+      for (var label in issue.labels.labels) {
+        if (label.label.contains('customer:')) {
+          hasCustomer = true;
+          break;
+        }
+      }
+      if (!hasCustomer) {
+        count--;
+      }
+    }
+  }
+  return count;
+}
+
+Duration meanDurationWithOrWithoutCustomers(List<dynamic> issues,
+    {bool onlyCustomers}) {
+  double sum = 0.0;
+  var count = issues == null ? 0 : issues.length;
+  if (count == 0) return Duration(seconds: 0);
+  bool hasCustomer = false;
+  for (var item in issues) {
+    var issue = item as Issue;
+    if (onlyCustomers)
+      for (var label in issue.labels.labels) {
+        if (label.label.contains('customer:')) {
+          hasCustomer = true;
+          break;
+        }
+      }
+    if (!onlyCustomers || (onlyCustomers && hasCustomer)) {
+      var delta = issue.closedAt.difference(issue.createdAt);
+      sum += delta.inSeconds;
+    }
+  }
+  if (count == 0)
+    return Duration(seconds: 0);
+  else {
+    int sumAsInt = sum.toInt();
+    int mean = sumAsInt ~/ count;
+    return Duration(seconds: mean);
   }
 }
 
@@ -59,10 +113,15 @@ void main(List<String> args) async {
 
   DateTime current = opts.from, last = opts.to;
 
-  if (opts.summarize)
+  if (opts.summarize) {
     print(
-        'Period ending\tCreated P0s\tCreated P1s\tCreated P2s\tClosed P0s\tClosed P1s\tClosed P2s');
-
+        'This shows the number of new, open, and closed `P0`, `P1`, and `P2` issues over the period from');
+    print('${opts.from} to ${opts.to}.');
+    if (opts.onlyCustomers)
+      print('Only issues with at least one `customer` label are presented.');
+    print(
+        'Period ending\tCreated P0s\tCreated P1s\tCreated P2s\tClosed P0s\tClosed P1s\tClosed P2s\tMean hours to close P1\tMean hours to close P2\tMean hours to close P3');
+  }
   while (current.isBefore(last)) {
     var next = current.add(Duration(days: 7));
     var fromStamp = current.toIso8601String().substring(0, 10);
@@ -81,8 +140,8 @@ void main(List<String> args) async {
     var openIssues = await github.searchIssuePRs(openQuery);
     var closedIssues = await github.searchIssuePRs(closedQuery);
 
-    var openedThisPeriod = List<Issue>();
-    var closedThisPeriod = List<Issue>();
+    List<Issue> openedThisPeriod = [];
+    List<Issue> closedThisPeriod = [];
 
     await for (var issue in openIssues) {
       if (issue.createdAt.compareTo(opts.from) >= 0 &&
@@ -103,34 +162,50 @@ void main(List<String> args) async {
     if (opts.summarize) {
       var openCount = HashMap<String, int>();
       var closeCount = HashMap<String, int>();
+      var mean = HashMap<String, Duration>();
       interestingPriorities.forEach((p) {
-        openCount[p] = openCluster.clusters[p] == null
+        var highPrioritizedIssuesOpened = openCluster[p];
+        var highPrioritizedIssuesClosed = closedCluster[p];
+        openCount[p] = highPrioritizedIssuesOpened == null
             ? 0
-            : openCluster.clusters[p].length;
-        closeCount[p] = closedCluster.clusters[p] == null
+            : countWithOrWithoutCustomers(highPrioritizedIssuesOpened,
+                onlyCustomers: opts.onlyCustomers);
+        closeCount[p] = highPrioritizedIssuesClosed == null
             ? 0
-            : closedCluster.clusters[p].length;
+            : countWithOrWithoutCustomers(highPrioritizedIssuesClosed,
+                onlyCustomers: opts.onlyCustomers);
+        mean[p] = meanDurationWithOrWithoutCustomers(
+            highPrioritizedIssuesClosed,
+            onlyCustomers: opts.onlyCustomers);
       });
       var row = '${toStamp}';
       interestingPriorities.forEach((p) => row = '${row}\t${openCount[p]}');
       interestingPriorities.forEach((p) => row = '${row}\t${closeCount[p]}');
+      interestingPriorities.forEach((p) => row = '${row}\t${mean[p].inHours}');
       print(row);
     } else {
       print(
           'This shows the number of new, open, and closed `P0`, `P1`, and `P2` issues over the period from');
       print('${fromStamp} to ${toStamp}.\n\n');
+      if (opts.onlyCustomers)
+        print('Only issues with at least one `customer` label are presented.');
 
       print('### Issues open/closed by priority\n');
       print('| Priority | Opened | Closed | Total |');
       print('|----------|--------|--------|-------|');
       var totalOpen = 0, totalClosed = 0, total = 0;
       interestingPriorities.forEach((p) {
-        var openCount = openCluster.clusters[p] == null
+        var highPrioritizedIssuesOpened = openCluster[p];
+        var highPrioritizedIssuesClosed = closedCluster[p];
+
+        var openCount = highPrioritizedIssuesOpened == null
             ? 0
-            : openCluster.clusters[p].length;
-        var closedCount = closedCluster.clusters[p] == null
+            : countWithOrWithoutCustomers(highPrioritizedIssuesOpened,
+                onlyCustomers: opts.onlyCustomers);
+        var closedCount = highPrioritizedIssuesClosed == null
             ? 0
-            : closedCluster.clusters[p].length;
+            : countWithOrWithoutCustomers(highPrioritizedIssuesClosed,
+                onlyCustomers: opts.onlyCustomers);
         var totalRow = openCount + closedCount;
         totalOpen += openCount;
         totalClosed += closedCount;
